4e97da148cc58779bbd22096a91831d86c414d73
i-Lucas <novo.contato.lucas@gmail.com>
2022-05-30 14:15:55 -0300

    change database settings

--- src/database/database.js
+++ src/database/database.js
@@ -2,15 +2,12 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+ 
 const { Pool } = pg;
 
 const databaseConfig = {
-    connectionString: process.env.DATABASE_URL
-}
-
-if (process.env.MODE === "PROD") { 
-    databaseConfig.ssl = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
         rejectUnauthorized: false
     }
 }
